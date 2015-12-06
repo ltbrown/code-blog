5a37788dec0470552a4ad4e9d3dac01bd7d715f1
Laurie Brown <ltbrown@mac.com>
2015-12-06 09:11:20 -0800

    functionality working

--- js/blog-admin.js
+++ js/blog-admin.js
@@ -3,7 +3,11 @@ var blog = blog || {};
 blog.admin = (function() {
 	'use strict';
 	// cached jquery objects
-	var $pLog = $('#pLog');
+	var $title = $('#title');
+   	var $category = $('#category');
+   	var $author = $('#author');
+  	var $authorUrl = $('#authorUrl');
+   	var $date = $('#date');
 
 	var $tBody    = $('#tBody');
   	var $pHrawOut = $('#pHrawOut');
@@ -11,7 +15,6 @@ blog.admin = (function() {
   	var $pJson    = $('#pJson');
 
 	// variables
-	var articleMeta = [];
 	var mObj = {}; // Empty object, filled in to during JSON string update
 	var self = {
 //initialize admin
@@ -28,17 +31,34 @@ blog.admin = (function() {
 				$('#submit-article').on('click', function(e) {
 					//console.log('hi');
 					e.preventDefault();
-					var title = $('#title').val();
-					var name = $('#name').val();
-					var url = $('#author-url').val();
-					var date = $('#date').val();
-					var category = $('#category').val();
-					articleMeta.push(title, name, url, date, category);
-					self.formatInputs(articleMeta);
-					//
+					//TODO: submit button will add jsonStr to blogArticles? or dynamically create blogArticles.js
 				});
 				 // Any character change (article editing) updates the live output paragraphs
-				$tBody.on('input', self.render);
+				$tBody.on('input', function(){
+					self.render();
+					$(this).removeClass('initial');
+				});
+
+				 // Any character change (input fields) updates the JSON output
+				$title.on('input', self.render);
+				$category.on('input', self.render);
+				$author.on('input', self.render);
+				$authorUrl.on('input', self.render);
+				$date.on('input', self.render);
+
+				$('#preview-article').on('click', function(e) {
+					//console.log('hi');
+					e.preventDefault();
+					self.resetArticlePreview();
+					self.makeArticlesHandlebars();
+				});
+				$('input').on('focus', self.resetArticlePreview);
+				$('input').on('input', function(){
+					$(this).removeClass('initial');
+				});
+			},
+			resetArticlePreview: function(){
+				$('#article-preview').empty();
 			},
 			render: function() {
 				var bodVal = $tBody.val(); // Raw article markup
@@ -47,53 +67,51 @@ blog.admin = (function() {
 			    $pMarkOut.html(m); // Render article preview (rendered as HTML)
 
 			    // Update JSON article
+			    if($title.val() == ""){
+			    	$title.val('New Title');
+			    }
+			    if($category.val() == ""){
+			    	$category.val('horses');
+			    }
+			    if($author.val() == ""){
+			    	$author.val('Author Name');
+			    }
+			    if($authorUrl.val() == ""){
+			    	$authorUrl.val('http:\/\/example.me.com');
+			    }
+			    if($date.val() == ""){
+			    	$date.val('2015\-11\-05');
+			    }
+			    mObj.title = $title.val();
+			   	mObj.category = $category.val();
+			   	mObj.author = $author.val();
+			  	mObj.authorUrl = $authorUrl.val();
+			   	mObj.publishedOn = $date.val();
 			    mObj.body = m;
 			    var jsonStr = $pJson.text(JSON.stringify(mObj));
 			},
+			// makeArticlesHandlebars
+			makeArticlesHandlebars: function() {
+				var template = $('#itemTemplate').html();
+				// Handlebars compiles the template into a callable function
+			      var renderer = Handlebars.compile(template);
+			      // put data in a variable
+			      var article = mObj;
+			      //console.log(article);
 
-			formatInputs: function(Meta) {
-					var keys = ["title", "category", "author", "authorUrl", "publishedOn"];
-				    var	result = [];
-				for ( var i = 0; i < keys.length; i++ ) {
-				  result.push( [ keys[i] + ": " + articleMeta[i] ] );
-				}
-				console.log("hi " + result);
-			}
+			      var result = renderer(article);
+			      //console.log('hi' + result);
+			      //format date before appending to to #articles
 
-		}
+			      $('#article-preview').append(result);
+			      //self.formatDate();
+			    // call init articles()
+				//self.initArticles();
+			},
+
+		};
 	return self;
 
 }());
 blog.admin.init();
 
-/*
-var tBody    = $('#tBody');
- var pHrawOut = $('#pHrawOut');
- var pMarkOut = $('#pMarkOut');
- var pJson    = $('#pJson');
- var mObj = {}; // Empty object, filled in to during JSON string update
- function render() {
-   var bodVal = tBody.val(); // Raw article markup
-   var m = marked(bodVal); // Convert markup to html
-   pHrawOut.text(m); // Render raw markup
-   pMarkOut.html(m); // Render article preview (rendered as HTML)
-   // Update JSON article
-   mObj.title = $('#title').val();
-   mObj.category = $('#category').val();
-   mObj.author = $('#author').val();
-   mObj.authorUrl = $('#authorUrl').val();
-   mObj.date = $('#date').val();
-   // mObj.title = 'hello ';
-   mObj.body = m;
-
-   var jsonStr = pJson.text(JSON.stringify(mObj));
- }
-
- // Any character change (article editing) updates the live output paragraphs
- $('#submitArticle').on('click', function(e){
-   e.preventDefault();
-   render();
- });
-render(); // Render once on doc load
-});
-*/
